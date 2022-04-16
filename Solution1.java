33fcc982ec2897d6d022887d23468a0876c5ded1
hahaminja <100562162+hahaminja@users.noreply.github.com>
2022-04-16 14:41:17 +0900

    Add files via upload

---
+++ Solution1.java
@@ -0,0 +1,33 @@
+
+public class Solution1 {
+	public static void main(String[] args) {
+	    
+	      // 정수 : int, 실수(소수) : double, 문장 : String
+			
+		int num1 = 100;
+		int num2 = 200;
+		
+		String str1 = "안녕하세요";
+		String str2 = "반갑습니다.";
+		
+		double dnum1 = 3.14;
+		double dnum2 = 10.5;
+		
+		
+		
+		
+	      System.out.println(num1); // 출력 : 100
+
+	      System.out.println(dnum1); // 출력 : 3.14
+
+	      System.out.println(str1); // 출력 : 안녕하세요
+
+	      System.out.println(num2); // 출력 : 200
+
+	      System.out.println(dnum2); // 출력 : 10.5
+
+	      System.out.println(str2); // 출력 : 반갑습니다.
+
+
+	  }
+}
